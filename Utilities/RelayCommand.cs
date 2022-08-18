b76597c41e148708356c1cb3b92d533a98a675e1
mikolajmarcinczak <mikolajtechhnika@gmail.com>
2022-08-18 19:29:42 +0200

    Observer implemented

---
+++ Utilities/RelayCommand.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace AccountManager.Utilities
+{
+    internal class RelayCommand<T> : ICommand
+    {
+        private readonly Action<T> _execute = null;
+        private readonly Func<T, bool> _canExecute = null;
+
+        public RelayCommand(Action<T> execute, Func<T, bool> canExecute)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute ?? (_ => true);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public bool CanExecute(object? parameter) => _canExecute((T)parameter);
+        public void Execute(object? parameter) => _execute((T)parameter);
+    }
+
+    public class RelayCommand : ICommand
+    {
+        readonly Action _execute;
+        readonly Func<bool> _canExecute;
+
+        public RelayCommand(Action execute, Func<bool> canExecute)
+        {
+            if (execute == null)
+            {
+                throw new NullReferenceException(nameof(execute));
+            }
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public RelayCommand(Action execute) : this(execute, null)
+        {
+
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public bool CanExecute(object? parameter) => _canExecute == null ? true : _canExecute();
+        public void Execute(object? parameter) => _execute.Invoke();
+    }
+}
--- ViewModels/LoginViewModel.cs
+++ ViewModels/LoginViewModel.cs
@@ -7,32 +7,75 @@ using System.Windows.Input;
 using AccountManager.Models;
 using AccountManager.Services.Implementations;
 using AccountManager.Services.Interfaces;
+using AccountManager.Utilities;
 
 namespace AccountManager.ViewModels
 {
     public class LoginViewModel
     {
+        private readonly MainWindow mainWindow;
         private readonly IUsersService usersService;
 
         public ICommand LoginCommand { get; }
         public ICommand RegisterCommand { get; }
 
-        public LoginViewModel()
+        public LoginViewModel(MainWindow mainWindow, IUsersService UsersService)
         {
-            this.usersService = usersService;
+            this.usersService = UsersService;
+            this.mainWindow = mainWindow;
+
+            LoginCommand = new RelayCommand(Login);
+            RegisterCommand = new RelayCommand(Register);
         }
 
-        private Login()
+        private void Login()
         {
-            var users = new User()
+            var user = new User()
             {
-                
+                Email = LoginMailInput.Text.Replace(Environment.NewLine, "").Replace("\n", ""),
+                Password = LoginPasswordInput.Text.Replace(Environment.NewLine, "").Replace("\n", "")
             };
+
+            var result = usersService.Login(user);
+
+            if (result == true)
+            {
+                Settings.Default.Email= user.Email;
+                mainWindow.Show();
+                Close();
+            }
+            else
+            {
+                statusLabel.Content = "Incorrect mail or password";
+            }
         }
 
-        private Register()
+        private void Register()
         {
+            var user = new User()
+            {
+                Email = RegisterMailInput.Text.Replace(Environment.NewLine, "").Replace("\n", ""),
+                Password = RegisterPasswordInput.Text.Replace(Environment.NewLine, "").Replace("\n", ""),
+                UserName = RegisterNameInput.Text.Replace(Environment.NewLine, "").Replace("\n", ""),
+                Surname = RegisterSurnameInput.Text.Replace(Environment.NewLine, "").Replace("\n", ""),
+                PhoneNumber = RegisterPhoneNumberInput.Text.Replace(Environment.NewLine, "").Replace("\n", "")
+            };
 
+            var result = usersService.Register(user);
+
+            if (result == true)
+            {
+                Settings.Default.Email = user.Email;
+                mainWindow.Show();
+                Close();
+            }
+            else
+            {
+                statusLabelReg.Content = "Error while signing up";
+            }
+
+            mainWindow.Show();
+            Close();
         }
     }
 }
